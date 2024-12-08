5f9fbb9bf2cf962f4c2f12b9c834579d26f4e31e
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-08 17:54:38 +0600

    added new problem topic 500 to 1000 difficulty problems

---
+++ 500 to 1000 difficulty problems/Greater_Average.cpp
@@ -0,0 +1,40 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2024-12-08 || 16:21:59
+ * * * * File    : Greater_Average.cpp
+ */
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--)
+    {
+        int a,b,c; cin >> a >> b >> c;
+        double avg=(a+b)/2.0;
+        if(avg>c){
+            yes;
+        }else no;
+    }
+    
+    return 0;
+
+
+// Alhamdulillah
+}
