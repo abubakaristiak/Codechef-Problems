d4bd814cd03de7fd655dbc8517a2e038972169e5
Abu Bakar Istiak <abubakar119147@gmail.com>
2024-12-28 19:56:15 +0600

    added new problem related to array

---
+++ Array/Duplicate_Integers.cpp
@@ -0,0 +1,48 @@
+/*
+ * Bismillahir Rahmanir Raheem
+ *
+ * * * * Coder   : abubakaristiak
+ * * * * Created : 2024-12-28 || 19:43:09
+ * * * * File    : Duplicate_Integers.cpp
+ */
+
+
+#include<bits/stdc++.h>
+#define ll long long
+#define pi pair<ll, ll>
+#define asort(v) sort(v.begin(), v.end())
+#define rsort(v) sort(v.begin(), v.end(), greater<>())
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+#define cyes cout << "Yes\n"
+#define cno cout << "No\n"
+#define endl "\n"
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+
+using namespace std;
+int main()
+{
+    fast();
+    int t; cin >> t;
+    while (t--)
+    {
+        int n; cin >> n;
+        vector<int> v(n);
+        set<int> st;
+        for(int i=0; i<n; i++){
+            cin >> v[i];
+            st.insert(v[i]);
+        }
+        if(n!=st.size()){
+            yes;
+        }else no;
+
+                
+
+    }
+    
+    return 0;
+
+
+// Alhamdulillah
+}
